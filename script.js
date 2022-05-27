5444aff762f243d4bafccddddfe3ef1ddc5f2e54
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-05-27 03:47:11 -0400

    consistency

--- script.js
+++ script.js
@@ -1,146 +1,162 @@
-var state = 'code';
+const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-var startEl = document.querySelector("#start");
-var quizEl = document.querySelector("#quiz");
-var endEl = document.querySelector("#end");
-var startBtn = document.querySelector("#start button");
-var quizTitle = document.querySelector("#quiz #title");
-var timerEl = document.querySelector("#countdown");
-var timeEl = document.querySelector("#time");
-var quizquestionsEl = document.querySelector("#questions");
-var secondsLeft = 60;
-var submitBtn = document.querySelector('#start');
 
-var position = 0;
-function displayState() {
-  if (state === 'start') {
-    startEl.style.display = 'block';
-    quizEl.style.display = 'none';
-    endEl.style.display = 'none';
-  }
-  if (state === 'quiz') {
-    startEl.style.display = 'none';
-    quizEl.style.display = 'block';
-    endEl.style.display = 'none';
-  }
-  if (state === 'end') {
-    startEl.style.display = 'none';
-    quizEl.style.display = 'none';
-    endEl.style.display = 'block';
-  }
+let shuffledQuestions, currentQuestionIndex
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
-function init() {
-  displayState();
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 
-startBtn.addEventListener("click", function() {
-  state = 'quiz';
-  displayState();
-});
+function selectAnswer() {
 
-quizTitle.addEventListener("click", function () {
-  state = 'end';
-  displayState();
-});
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
-    function showQuestions(questions, quizContainer) {
-
-    }
-    function showResults(questions, quizContainer, resultsContainer) {
-
-    }
-    showQuestions(questions,quizContainer,resultsContainer);
 }
 
-var quizquestionsEl = [
+
+
+
+const questions = [
     {
-        Question1: "How many seconds are in a day?",
-        answers: {
-            a: '46,800',
-            b: '86,400',
-            c: '64,800'
-        },
-        correctAnswer: 'B'
-    },
-    {
-        Question2: "When was the first programming language developed?",
-        answers: {
-            a: '1983',
-            b: '1383',
-            c: '1883'
+        question: 'How many seconds are in a day?',
+        answers: [
+            {text: '46,800', correct: false},
+            {text: '86,400', correct: true}
 
-        },
-        correctAnswer: 'C'
-    },
-    {
-        Question3: "When was the first website introduced to the world?",
-        answers: {
-            a: '1991',
-            b: '2001',
-            c: '1919'
-        },
-        correctAnswer: 'A'
-    }
-    ];
+            ]
 
+        }
+]
 
-function showQuestions(questions, quizContainer) {
-    var output = [];
-    var answers;
+    // function setNextQuestion() {
+    //     resetState()
+    //     showQuestions(shuffledQuestions[currentQuestionIndex])
+    // }
 
-    for(var i=0; i<questions.length; i++) {
-
-        answers = [];
-
-    for(letter in questions[i].answers) {
-
-        answers.push(
-            '<label>'
-            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-            + letter + ':'
-            + questions[i].answers[letter]
-        + '</label>'
-
-        );
-    }
-
-}
-}
-
-output.push(
-    '<div class="question">' + questions[i].question + '</div>'
-    + '<div class="answers">' + answers.join('') + '</div>'
-);
-
-Container.innerHTML = output.join('');
-
-showQuestions(questions,quizContainer);
-
-function showResults(questions, quizContainer, resultsContainer) {
-
-    var answerContainers = quizContainerl.querySelectorAll('.answers');
-
-    var userAnswer = '';
-    var numCorrect = 0;
-
-    
-    for(var i=0; i<questions.length; i++) {
+    // function showQuestions(question) {
+    // questioneElement.innerText = question.question
+    // question.answers.forEach(answer => {
+    //     const button = document.createElement('button')
+    //     button.innerText = answer.text
+    //     button.classList.add('btn')
+    //     if (answer.correct) {
+    //         button.dataset.correct = answer.correct
+    //     }
+    //     button.addEventListener('click', selectAnswer)
+    //     answerButtonsElement.appendChild(button)
+    // })
+    // }
 
 
-    userAnswer = (answerContainers[i].querySelector('input[name=question'))
-    }
-}
+    // function startGame() {
+    //     console.log('Started')
+
+    // }startButton.classList.add('hide')
+    // questionContainerElement.classList.remove('hide')
 
 
-function pushToLocalStorageArray() {
-    var arr = JSON.parse(localStorage.getItem('array')) || [];
-    arr.push('val');
+    // function setNextQuestion() {
 
-    localStorage.setItem('array', JSON.stringify(arr));
-    console.log(JSON.parse(localStorage.getItem('array')));
-}
-init();
+
+    // }
+
+    // function resetState() {
+    //     nextButton.classList.add('hide')
+    //     while (answerButtonsElement.firstChild) {
+    //         answerButtonsElement.removeChild
+    //         (answerButtonsElement.firstChild)
+    //     }
+    // }
+    // function selectAnswer(e) {
+    //     const selectedButton = e.target
+    //     const correct = selectedButton.dataset.correct
+    //     setStatusClass(document.body, correct)
+    //     Array.from(answerButtonsElement.children).forEach(button => {
+    //         setStatusClass(button,button.dataset.correct)
+
+    //     })
+    //     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    //         nextButton.classList.remove('hide')
+    //     } else {
+    //             startButton.innerText = 'Restart'
+    //             startButton.classList.remove('hide')
+    //         }
+
+    //     }
+
+        
+
+
+    // function setStatusClass(element, correct) {
+    //     clearStatusClass(element)
+
+    //     if (correct) {
+    //         element.classList.add('correct')
+    //     } else {
+    //         element.classList.add('wrong')
+    //     }
+    // }
+
+    // function clearStatusClass(element) {
+    //     element.classList.remove('correct')
+    //     element.classList.remove('wrong')
+    // }
+    //     if (correct) {
+    //         element.classList.add('correct')
+    //     } else {
+    //         element.classList.add('wrong')
+    //     }
+
+
+
+    // const questions = [
+
+    // { 
+    //     Question1: "How many seconds are in a day?",
+    //     answers: [
+    //     {a: '46,800', correct: false},
+    //     {b: '86,400', correct: true }
+    //     ]
+    // }
+
+    // {  
+    //         Question2: "When was the first programming language developed?",
+    //     answers: {
+    //         a: '1983',
+    //         b: '1383',
+    //         c: '1883'
+
+    //     },
+    //     correctAnswer: 'C'
+    // },
+    // {
+    //     Prompt3: "When was the first website introduced to the world?",
+    //     answers: {
+    //         a: '1991',
+    //         b: '2001',
+    //         c: '1919'
+    //     },
+    //     correctAnswer: 'A'
+    // }
+
+    // ];
+
+
+
+
